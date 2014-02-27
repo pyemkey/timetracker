2cc4fdc293e694364d4efb8ea39f740080133d69
pyemkey <py.emkey@gmail.com>
2014-02-27 15:13:06 +0100

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140227134951) do
+ActiveRecord::Schema.define(version: 20140227141133) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "company_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
