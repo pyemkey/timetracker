b6b8b7f2c52935fe29bc6ec59da568798f3e500d
pyemkey <py.emkey@gmail.com>
2014-02-27 18:23:58 +0100

    Add scopes to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   belongs_to :company
   has_many :works
   has_many :users, through: :works
+
+  scope :low_default_rate, -> { where("default_rate < ?", 100)}
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,7 @@
 class Work < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  scope :fullday, -> { where("hours >= 8") }
+  scope :recent, -> { where("date_time_performed > '#{Time.now - 7.days}'")}
 end
