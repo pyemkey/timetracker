de06d2039f537e265f6a6cd9df042b3921ea2c53
pyemkey <py.emkey@gmail.com>
2014-02-28 16:00:41 +0100

    Add validation for slugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 5 }
   validates_presence_of :company
   validates :default_rate, numericality: { only_integer: true, greater_than_or_equal_to: 50 }
-
+  validates :slug, uniqueness: true, length: { minimum: 3 }
 
   scope :low_default_rate, -> { where("default_rate < ?", 100)}
 end
