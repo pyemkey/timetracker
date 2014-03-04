3ca2afed64367e033bdf4eefe2351b8335c989dd
pyemkey <py.emkey@gmail.com>
2014-03-04 23:14:10 +0100

    Add relation between user and project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   belongs_to :company
   has_many :works
   has_many :users, through: :works
+  belongs_to :user
 
   validates :name, presence: true, length: { minimum: 5 }
   validates_presence_of :company
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :company
   has_many :works
   has_many :projects, through: :works
-
+  has_many :projects
+  
   validates :first_name, length: { minimum: 2}
   validates :last_name, length: { minimum: 5}
   validates_presence_of :company
