9379a9f9fe2e9ec14d85faa281d2527fe94b42e1
pyemkey <py.emkey@gmail.com>
2014-03-05 23:29:20 +0100

    Make readable csv

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,9 +17,16 @@ class Project < ActiveRecord::Base
 
   def self.export_csv projects
     CSV.generate() do |csv|
-      csv << column_names
+      csv << ["name", "company", "default_rate", "created_at", "owner", "most_recent_work_item"]
       projects.each do |project|
-        csv << project.attributes.values_at(*column_names)
+        csv << [
+                project.name,
+                project.company,
+                project.default_rate,
+                project.created_at,
+                project.user,
+                project.works.last
+                ]
       end
     end
   end
