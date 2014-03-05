ca9bba5e2a8f5311ca6a986b70592e3fbfc72cca
pyemkey <py.emkey@gmail.com>
2014-03-05 19:13:59 +0100

    Export csv project data

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,4 +14,13 @@ class Project < ActiveRecord::Base
   def to_s
     "#{name} (#{company.name})"
   end
+
+  def self.export_csv projects
+    CSV.generate() do |csv|
+      csv << column_names
+      projects.each do |project|
+        csv << project.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
