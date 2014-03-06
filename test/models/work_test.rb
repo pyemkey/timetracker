cd8344c0908f660be1c3f9642c270982ccb8ea7a
pyemkey <py.emkey@gmail.com>
2014-03-06 21:51:48 +0100

    Add unit-test for work

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -4,4 +4,19 @@ class WorkTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "work should not save with no associated user" do
+    work = Work.first
+    assert work.valid?
+    work.user_id = nil
+    assert_not work.valid?
+  end
+
+  test "work should not save with future date" do
+    work = Work.first
+    assert work.valid?, "work should be valid"
+    work.date_time_performed = Date.today + 2.days
+    assert work.invalid?, "work should be invalid"
+    assert work.errors[:date_time_performed].any?, "work should have date time performed error"
+  end
 end
