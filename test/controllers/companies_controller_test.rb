04c1b8ea94bfa21799462b739a9de8dfd91eb100
pyemkey <py.emkey@gmail.com>
2014-03-06 21:51:27 +0100

    Add simple functional tests

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CompaniesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  include Devise::TestHelpers
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:companies)
+  end
 end
--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  include Devise::TestHelpers
+
+  test "user can't view projects" do
     get :index
-    assert_response :success
-  end
+    assert_response :redirect, "un-logged-in user should be redirected" 
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+    @user = users(:michalk) 
 
+    sign_in @user
+
+    get :index
+    assert_response :success, "logged in user should access index"
+
+    sign_out @user
+
+    get :index
+    assert_response :redirect, "logged_out user should be redirected"
+  end
 end
